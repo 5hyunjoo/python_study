42cd5d416d56242207b83546e65d47f70d12ca11
Oh hyun joo <hjhelen071227@naver.com>
2022-02-20 20:13:55 +0900

    first commit

---
+++ 3-1.py
@@ -0,0 +1,81 @@
+# print(1+1)
+# print(3-2)
+# print(5*2)
+# print(6/3)
+
+# print(2**3) #2^3
+# print(5%3) #2
+# print(10%3) #1
+# print(5//3) #1
+# print(10//3) #3
+
+# print(10 > 3) #True
+# print(4 >= 7) #False
+
+# variable = 4
+# print(variable) #True
+
+# print(1 == 3) #false
+# print(1 != 3) #true
+# print(not 1 == 3) #true
+
+# print(True & True) #true 
+# print(True and False) #false
+# print(False and False) #false
+
+# print(True | True) #true
+# print(True or False) #true
+# print(False or False) #false
+
+# print(5 > 4 > 3)
+# print(5 > 4 & 4 > 3)
+
+# print(5 > 4 > 7)
+# print(5 > 4 & 4 > 7)
+
+# Max = max(5, 12, 14)
+# from math import *
+# print(Max)
+# print(floor(4.99))
+# print(ceil(3.14))
+
+# # number = number + 2
+# number
+# # number = -2
+
+# print(number)
+
+from random import *
+
+# print(int(random()*100)%42 + 1)
+# print(int(random()*100)%42 + 1)
+# print(int(random()*100)%42 + 1)
+# print(int(random()*100)%42 + 1)
+# print(int(random()*100)%42 + 1)
+# print(int(random()*100)%42 + 1)
+
+# print(int(random() * 45) + 1)
+# print(int(random() * 45) + 1)
+# print(int(random() * 45) + 1)
+# print(int(random() * 45) + 1)
+# print(int(random() * 45) + 1)
+# print(int(random() * 45) + 1)
+# print(int(random() * 45) + 1)
+# date = randint(4,28)
+
+# date = (int(random()*25)+4)
+# print("오프라인 스터디 모임 날짜는 매월 "+ str(date) + "일로 선정되었습니다.")
+
+# sentence = "현주는 \n16살입니다."
+# print(sentence)
+
+# sent = '현주는\n 신정여중에 다닙니다.'
+
+# print(sent)
+
+# sent2 = """
+# 현주는\n 여자입니다.
+# """
+# print(sent2)
+
+print((int(random()*30)+1)*2)
---
+++ first_python.py
@@ -0,0 +1,17 @@
+# 애완동물을 소개해 주세요!
+
+name = "연탄이"
+animal = "강아지"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+'''
+여러문장 주석
+
+'''
+
+
+print("우리집" + animal + "의 이름은 "+name+"예요")
+print(name + "는 "+ str(age) + "살이며, "+ hobby + "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
